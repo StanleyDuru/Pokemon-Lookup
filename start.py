85fa2a60f575eecedcbcab1e7e499c2fa50dd53d
Erica Lee <elee006@odu.edu>
2021-07-08 16:59:47 +0000

    printed data fixed
    Co-authored-by: Stanley Duru <durustan@msu.edu>

--- start.py
+++ start.py
@@ -5,10 +5,10 @@ import sqlalchemy as db
 from sqlalchemy import create_engine
 from sqlalchemy import select
 
+
+
+
 #response = requests.request("GET", url, headers=headers)
-
-
-
 #have an input statement somewhere
 #   2 input statements:
 #       input for if you want pokemon:
@@ -21,33 +21,17 @@ from sqlalchemy import select
 #           gets json for each pokemon with corresponding stats
 #           create database and store it
 
+#gets the status code of the json()    
+def status(url, headers):
+    response = requests.request("GET", url, headers=headers)
+    code = response.status_code
+    return code
 
-
-#def pokemon(input):
-   # print(select(df.Pokemon))
- #   db= pymysql.connect("localhost","testuser","admin","TESTDB")
-  #  cursor =db.cursor()
-   # cursor.execute("SELECT name, base_stamina, base_defense, base_attack FROM Pokemon_stats WHERE name=%s", (input,))
-    #data = cursor.fetchall()
-    #if data:
-     #   print("Pokemon exists")
-    #else:
-     #   print("Pokemon does not exist")
-        
-    
-#def stat(input):
-
-    
-
-    
-def status(response):
-    return response.status_code
-
-
+#gets the data from the json()
 def get_json(url, headers):
     response = requests.request("GET", url, headers=headers)
     return response.json()
-
+#creates a list of only Pokemon with only the form: 'Normal'
 def create_lis(rep):
     lis = []
     for i in rep:
@@ -55,31 +39,39 @@ def create_lis(rep):
             lis.append(tuple((i['pokemon_name'],i['base_stamina'],i['base_defense'],i['base_attack'])))
     return lis
     
-    
+#Creates the dataframe of all the Pokemon    
 def dataframe(lis):
     col_names = ['Name', 'Base_stamina', 'Base_defense', 'Base_attack']
     df = pd.DataFrame(columns = col_names)
     for i in lis:
         df.loc[len(df.index)] = [str(i[0]),int(i[1]),int(i[2]),int(i[3])]
     return df
-
+#Creates a database table of all the Pokemon
 def makeSqlTable(df, database_name, table_name):
     engine = db.create_engine("mysql://root:codio@localhost/Pokemon?charset=utf8")
     os.system('mysql -u root -pcodio -e "CREATE DATABASE IF NOT EXISTS '+database_name+';"')
     df.to_sql(table_name, con=engine, if_exists='replace', index=False)
     return engine
-
+#Gets the data for the Pokemon the user entered
 def fetch_pokemon(engine,table_name, pokemon):
+    col_names = ['Name', 'Base_stamina', 'Base_defense', 'Base_attack']
     connection = engine.connect()
     metadata = db.MetaData()
     c = db.Table(table_name, metadata, autoload=True, autoload_with=engine)
     query = db.select([c]).where(db.and_(c.columns.Name == pokemon))
     ResultProxy = connection.execute(query)
     ResultSet = ResultProxy.fetchall()
-    return ResultSet
+    df = pd.DataFrame(ResultSet, columns = col_names)
+    if df.empty:
+        error = "\n\tNo Results Found\n"
+        return error
+    else:
+        print("\nResults for {}:".format(pokemon))
+        return df
 
-
+#Gets the data for the stat and value the user input
 def fetch_stat(engine,table_name, stat, val):
+    col_names = ['Name', 'Base_stamina', 'Base_defense', 'Base_attack']
     connection = engine.connect()
     metadata = db.MetaData()
     c = db.Table(table_name, metadata, autoload=True, autoload_with=engine)
@@ -91,13 +83,20 @@ def fetch_stat(engine,table_name, stat, val):
         query = db.select([c]).where(db.and_(c.columns.Base_defense == val))
     ResultProxy = connection.execute(query)
     ResultSet = ResultProxy.fetchall()
-    return ResultSet
+    df = pd.DataFrame(ResultSet, columns = col_names)
+    if ResultSet == []:
+        error = "\tNo Results Found"
+        return error
+    else:
+        print("\nPokemon Found:")
+        return df
 
 
 
 
 def main():
     url = "https://pokemon-go1.p.rapidapi.com/pokemon_stats.json"
+
     headers = {
         'x-rapidapi-key': "cdcc19f7bemshcaafa6b12f20c45p1ec5eajsn982786737a25",
         'x-rapidapi-host': "pokemon-go1.p.rapidapi.com"
@@ -105,17 +104,24 @@ def main():
     database_name = 'Pokemon'
     table_name = 'Pokemon_stats'
     rep = get_json(url,headers)
+    status(url, headers) 
+    print(status)
     info = create_lis(rep)
     df = dataframe(info)
     data = makeSqlTable(df, database_name, table_name)
-    
+ #Prompt for user input   
+    Prompt = "\n\t ***Welcome to Pokemon GO Pokemon Finder Project***\n\n\
+        You will be prompted to either look for your pokemon\n \
+        \tor find pokemon with stats you prefer!\n" 
+        
+    print(Prompt)
     choice = input("\nSelect to input:\n\t 1. Pokemon\n\t 2. Stat\n\n\t type '0' to exit\n")
     while choice != '0':
         #choices= ['1','2']
         while not(choice == '1' or choice == '2'):
             print("\n***Invalid Input. Please try again!***\n")
             break
-            #choice = input("\nSelect to input:\n\t 1. Pokemon\n\t 2. Stat\n\n\t type '0' to exit\n")
+            #choice = input("\nSelect to input:\n\t 1. Pokemon\n\n\t 2. Stat\n\n\t type '0' to exit\n")
             
 
         while choice == '1'or choice == '2':
@@ -125,15 +131,18 @@ def main():
                     print (res)
                     break
             if choice == '2':
-                    print("\nChoose from the following base stats:\n 1. Stamina \n 2. Defense \n 3. Attack\n")
+                    print("\nChoose from the following base stats:\n\t 1. Stamina \n\t 2. Defense \n\t 3. Attack\n")
                     stat = input()
+                    while not(stat == '1' or stat == '2' or stat == '3'):
+                        print('\n***Invalid stat. Try again!***\n')
+                        stat= input()
                     print("\nInput a value for the stat selected\n")
                     val = input()
                     res = fetch_stat(data,table_name, stat, val)
                     print(res)
                     break
         
-        choice = input("\nSelect to input:\n\t 1. Pokemon\n\t 2. Stat\n\t type '0' to exit\n")
+        choice = input("\nSelect to input:\n\t 1. Pokemon\n\t 2. Stat\n\n\t type '0' to exit\n")
         
     while choice == '0':
         print("\n Thank You for your time, See you later!\n")
@@ -143,9 +152,8 @@ def main():
 if __name__ == "__main__":
     main()
 
-#s = select(users)
-#conn = data.connect()
-#result = conn.execute(s)
+
+
 
 
 
